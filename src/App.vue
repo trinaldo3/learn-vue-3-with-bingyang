ba35ba77f69d66579c4b343029b0ff850a355c5e
Tucker Rinaldo <tuckerrinaldo@gmail.com>
2025-02-24 07:49:26 -0600

    Reactivity system

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,76 @@
 <template>
-  <div>
-    <div>Hello, Vue!</div>
+  <div class="card">
+    <h2>Message: {{ message }}</h2>
+    <h2>Number: {{ number }}</h2>
+    <button @click="changeMessageToUpperCase">
+      Change message to upper case
+    </button>
+    <button @click="incrementNumber">Increment number</button>
+  </div>
+  
+  <div class="card">
+    <h2>Name: {{ wizard.name }}</h2>
+    <h2>Wand: {{ wizard.wand }}</h2>
+    <button @click="changeNameToUpperCase">Change Name to Upper case</button>
+    <button @click="changeWandCore">Change wand core</button>
+    <button @click="changeWizard">Change Wizard</button>
+  </div>
+  <div class="card">
+    <h2>Array: {{ wizards }}</h2>
+    <button @click="wizards.push('Draco')">Add a new wizard</button>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+let message = ref('Hello, Reactivity!')
+let number = ref(1)
 
+function changeMessageToUpperCase() {
+  message.value = message.value.toUpperCase()
+  console.log(message)
+}
+
+function incrementNumber() {
+  number.value += 1
+  console.log(number)
+}
+
+let wizard = ref({
+  id: 1001,
+  name: 'Harry Potter',
+  house: 'Gryffindor',
+  age: 17,
+  wand: {
+    core: 'Pheonix Feather',
+    wood: "Holy"
+  }
+})
+
+function changeNameToUpperCase() {
+  wizard.value.name = wizard.value.name.toUpperCase()
+}
+
+function changeWandCore () {
+  wizard.value.wand.core = 'Unicorn Hair'
+}
+
+function changeWizard() {
+  wizard.value = {
+    id: 1002,
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Dragon heartstring',
+      wood: 'Vine'
+    }
+  }
+}
+
+let wizards = ref(['Harry', 'Hermione', 'Ron'])
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
