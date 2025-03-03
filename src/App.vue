a61439d5821b16bf6f93b75029085d418667efc3
Tucker Rinaldo <tuckerrinaldo@gmail.com>
2025-03-03 15:30:15 -0600

    lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <h1>{{ message }}</h1>
   <div class="card">
     <h2 ref="title">This is the App componenet.</h2>
-    <!--<h2>Number: {{ number }}</h2> -->
+    <h2>Number: {{ number }}</h2>
     <button @click="number++">Increment Number by one</button>
+    <button @click="isShow = !isShow">Toggle component1</button>
+    <Component1 v-if="isShow"></Component1>
   </div>
 </template>
 
 <script setup>
-import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, ref } from 'vue'
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onWatcherCleanup, ref, watch } from 'vue'
+
+import Component1 from './Component1.vue'
 
 let message = ref('Hello, from Lifecycel Hooks!')
 
 let number = ref(1)
-let title = ref()
+let title = ref(null)
+
+let isShow = true
 
 console.log('App component is setup')
 
@@ -42,6 +48,10 @@ onBeforeUnmount(() => {
 
 onUnmounted(() => {
   console.log('App componnent is unmointed.')
+})
+
+watch(number, () => {
+  console.log('Number changed!')
 })
 </script>
 
---
+++ src/Component1.vue
@@ -0,0 +1,36 @@
+<template>
+    <div style="background-color: aqua; padding: 10px; margin: 20px;">
+        <h2>This is the Component1 component.</h2>
+    </div>
+</template>
+
+<script setup>
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onWatcherCleanup, ref, watch } from 'vue'
+
+console.log('Component1 is setup')
+
+onBeforeMount(() => {
+  console.log('componenent1 is before mount.')
+})
+
+onMounted(() => {
+  console.log('componenet1 is mounted')
+})
+
+onBeforeUpdate(() => {
+  console.log('componnent1 is before update.')
+})
+
+onUpdate(() => {
+  console.log('componnent1 is updated.')
+})
+
+onBeforeUnmount(() => {
+  console.log('componnent1 is before unmount.')
+})
+
+onUnmounted(() => {
+  console.log('componnent1 is unmointed.')
+})
+
+</script>
