23de38abbafb48af214a1c3e485431cf465e496b
Tucker Rinaldo <tuckerrinaldo@gmail.com>
2025-03-08 13:15:16 -0600

    vue-components-events(child to parent)

---
+++ src/BlogPost.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="blog-post">
+      <p>
+        {{ message }}
+      </p>
+      <!-- Display the id, title, and content of a blog post. -->
+      <h2>{{ id }} - {{ blogPostTitle }}</h2>
+      <h4>{{ blogPostContent }}</h4>
+      <button @click="$emit('delete-blog-post', id)">Delete Post</button>
+      <button @click="emitDeletePostEvent(id)">Delete Post</button>
+
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  let message = ref('This is the BlogPost component.')
+  defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+  const emit = defineEmits(['delete-blog-post']) // optional but recommended
+
+  function emitDeletePostEvent(id) {
+    emit('delete-blog-post', id)
+  }
+  </script>
+  
+  <style scoped>
+  .blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+  </style>
