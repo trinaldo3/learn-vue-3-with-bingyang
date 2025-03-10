69df94ed8dbcf0de1e1f008b88f4415045d3e903
Tucker Rinaldo <tuckerrinaldo@gmail.com>
2025-03-10 08:39:57 -0500

    vue named slots

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <div>Hello, Vue!</div>
-  </div>
+  <BaseLayout>
+    <template #header>
+      <h1>Here might be a page title.</h1>
+    </template>
+    <template #main>
+      <p>A paragraph of the main content.</p>
+      <p>And another one.</p>
+    </template>
+    <template #footer>
+      <p>Here's some contact info.</p>
+    </template>
+  </BaseLayout>
 </template>
 
 <script setup>
-
+import BaseLayout from './BaseLayout.vue'
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
