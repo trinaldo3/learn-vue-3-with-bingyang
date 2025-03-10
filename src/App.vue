13858ac9ed575a3a7770c58672c1b279e53b2f15
Tucker Rinaldo <tuckerrinaldo@gmail.com>
2025-03-10 08:36:11 -0500

    slots intro

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <div>Hello, Vue!</div>
-  </div>
+  <AlertBox>Invalid username or password. Please try again.</AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+    <h1>The request timed out. Please refresh the page and try again.</h1>
+  </AlertBox>
 </template>
 
 <script setup>
-
+import AlertBox from './AlertBox.vue'
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style  scoped></style>
