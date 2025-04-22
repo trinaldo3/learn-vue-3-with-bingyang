2a3fa11830db5c711ab01d76796c7f3d01ca9237
Tucker Rinaldo <tuckerrinaldo@gmail.com>
2025-04-21 19:22:40 -0500

    transitions

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,66 +1,43 @@
 <template>
-    <div class="blog-post-container">
-        <div v-if="loading">Loading...</div>
-        <div v-else-if="blogPost">
-            <div>Blog Id: {{ $route.params.id }}</div>
-            <h1 class="blog-title">{{ blogPost.title }}</h1>
-            <div class="blog-content">
-                <p>{{ blogPost.content }}</p>
-            </div>
-        </div>
-        <div v-else>Failed to load blog post. Please try again later.</div>
+    <div class="blog-posts-layout">
+        <aside class="blog-post-list">
+            <BlogPostList></BlogPostList>
+        </aside>
+        <main class="blog-post-content-with-sidebar">
+            <!-- This is the default router-view -->
+            <router-view class="blog-post-content" v-slot="{ Component, route }">
+                <transition mode="out-in"
+                    enter-active-class="animate__animated animate__rotateInDownLeft animate__faster"
+                    leave-active-class="animate__animated animate__rotateOutUpRight animate__faster">
+                    <component :is="Component" :key="route.path"></component>
+                </transition>
+            </router-view>
+            <!-- This is the named router-view -->
+            <router-view name="sidebar"></router-view>
+        </main>
     </div>
 </template>
 
 <script setup>
-import api from '@/apis/blogPosts'
-import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-
-const blogPost = ref(null)
-const loading = ref(true)
-const route = useRoute()
-
-async function loadBlogPost(id) {
-    try {
-        blogPost.value = await api.findById(id)
-    } catch (error) {
-        console.error(`Failed to load blog post with id ${id}`, error)
-    } finally {
-        loading.value = false
-    }
-}
-
-watch(() => route.params.id, (newId, oldId) => {
-    loadBlogPost(newId)
-}, { immediate: true })
+import BlogPostList from '@/components/BlogPostList.vue'
 </script>
 
 <style lang="scss" scoped>
-.blog-post-container {
-    padding: 1.5rem;
-    background-color: #f5f5f5;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    margin: 0 auto;
+.blog-posts-layout {
+    display: flex;
+    width: 100%;
+    gap: 1rem;
 
-    .blog-title {
-        font-size: 2rem;
-        font-weight: bold;
-        color: #333;
-        margin-bottom: 1rem;
-        text-align: center;
+    .blog-post-list {
+        flex: 0 0 25%
     }
 
-    .blog-content {
-        font-size: 1.1rem;
-        line-height: 1.6;
-        color: #555;
-        text-align: justify;
+    .blog-post-content-with-sidebar {
+        display: flex;
+        flex: 0 0 75%;
 
-        p {
-            margin-bottom: 1rem;
+        .blog-post-content {
+            flex: 1; // Take up the remaining space
         }
     }
 }
